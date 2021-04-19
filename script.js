3e3d70321cafcffdce601cd7bde636ac5d0a4c6e
kwgn <kawaygun@gmail.com>
2021-04-19 22:20:29 +0300

    1st 19.04.2021

---
+++ script.js
@@ -0,0 +1,33 @@
+const person = {
+    name: "Alexander",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name); //идентичны 2 варианта, но 1 правильный
+//console.log(person["name"]);
+
+let ImgArr = ["apple.png", "car.jpg", 8, {}, []]; //в массиве могут быть другие объекты
+console.log(ImgArr[4]);
+
+//const result = confirm("Are you here?"); //как алерт с 2-мя вариантами ответа
+//console.log(result); //принимает true or false
+
+//const answer = prompt("У тебя есть хата на НГ?", "Нету:("); //вторые кавычки - дефолт, который будет заранее находиться в строке
+//console.log(answer); //аларм с инпутом, вход - стринг
+
+//const numAnswer = +prompt("У тебя есть хата на НГ?", "Нету:("); //+ это вариант динамической типизации, преобразующий вход в тип number
+//console.log(typeof(numAnswer));
+
+/*const answers = [];
+
+answers[0] = prompt("Ваше имя?", "");
+answers[1] = prompt("Ваша фамилия?", "");
+answers[2] = prompt("Ваш возраст?", "");
+
+document.write(answers); //выводит массив, заполненный пользователем*/
+
+const category = "toys"; //метод интерполяции, работает только с `такими кавычками`
+const url = "https://";
+console.log(`${url}someurl.com/${category}/5`);
+alert(`We need to buy some ${category}`);
