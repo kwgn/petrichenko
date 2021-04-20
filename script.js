f147dc138932340c29373c89f1b1a5d1c9537444
kwgn <kawaygun@gmail.com>
2021-04-20 20:45:37 +0300

    20.04.2021

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const person = {
+/*const person = {
     name: "Alexander",
     age: 25,
     isMarried: false
@@ -8,7 +8,7 @@ console.log(person.name); //идентичны 2 варианта, но 1 пра
 //console.log(person["name"]);
 
 let ImgArr = ["apple.png", "car.jpg", 8, {}, []]; //в массиве могут быть другие объекты
-console.log(ImgArr[4]);
+console.log(ImgArr[4]);*/
 
 //const result = confirm("Are you here?"); //как алерт с 2-мя вариантами ответа
 //console.log(result); //принимает true or false
@@ -27,7 +27,26 @@ answers[2] = prompt("Ваш возраст?", "");
 
 document.write(answers); //выводит массив, заполненный пользователем*/
 
-const category = "toys"; //метод интерполяции, работает только с `такими кавычками`
-const url = "https://";
-console.log(`${url}someurl.com/${category}/5`);
-alert(`We need to buy some ${category}`);
+//const category = "toys"; //метод интерполяции, работает только с `такими кавычками`
+//const url = "https://";
+//console.log(`${url}someurl.com/${category}/5`);
+//alert(`We need to buy some ${category}`);
+
+//const answers = [];
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat: false
+};
+
+let lastMovie = prompt("Один из последних просмотренных фильмов?", "");
+let lastMovieMark = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[lastMovie] = lastMovieMark; //работа через квадратные скобки защищает от ошибок
+
+console.log(personalMovieDB.movies);
