52e40a2a856aecba380b0449cb4567eed67b5361
kawaygun <kawaygun@gmail.com>
2021-07-05 20:15:37 +0300

    work2

--- script.js
+++ script.js
@@ -28,11 +28,35 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let lastMovie = prompt("Последний фильм?", 'Железный человек');
-    let lastMovieMark = +prompt("Дай ему оценку", '8');
-    personalMovieDB.movies[lastMovie] = lastMovieMark;
+let i = 0;
+while (i < 2) {
+    let lastMovie = prompt("Последний фильм?", 'Железный человек'),
+        lastMovieMark = +prompt("Дай ему оценку", '8');
+    if (lastMovie != '' && lastMovie != null && lastMovie != undefined && lastMovie.length <= 50 && !isNaN(lastMovieMark)) {
+        personalMovieDB.movies[lastMovie] = lastMovieMark;
+        i++;
+    }
 }
 
-console.log(personalMovieDB);
+/* пример из урока (выше сам решил)
+    for (let i = 0; i < 2; i++) {
+        const a = promt('Один из последних просмотренных фильмов?', ''),
+              b = promt('На колько оцените его?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
+*/
 
+console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+    console.log("Мало");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Нормально");
+} else if (personalMovieDB.count > 30) {
+    console.log("Много");
+} else {
+    console.log("Косяк");
+}
