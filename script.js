7ba86b95b6594998551fdd36216dc26c2c3990e4
kawaygun <kawaygun@gmail.com>
2021-07-05 19:02:47 +0300

    work1

--- script.js
+++ script.js
@@ -3,6 +3,21 @@
 const a = 100;
 (a === 500) ? console.log('yes'): console.log('no');
 
+switch (a) {
+    case 99:
+        console.log(a);
+        break;
+    case 101:
+        console.log(a);
+        break;
+    case 1000:
+        console.log(a);
+        break;
+    default:
+        console.log('Нет совпадений');
+        break;
+}
+
 const numberOfFilms = +prompt("Сколько фильмов посмотрел?", '1');
 
 const personalMovieDB = {
@@ -11,7 +26,7 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-}
+};
 
 for (let i = 0; i < 2; i++) {
     let lastMovie = prompt("Последний фильм?", 'Железный человек');
@@ -20,3 +35,4 @@ for (let i = 0; i < 2; i++) {
 }
 
 console.log(personalMovieDB);
+
